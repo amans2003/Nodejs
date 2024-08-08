b62a0fc43f78e68370b3f8d456a8d909ae7f6f6a
Aman Singh <140417719+amans2003@users.noreply.github.com>
2024-08-08 09:43:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ function myHandler(req , res) {
 } 
 app.listen(8000, () => console.log("Server Started!"));
 
-
+# Express makes code easier and clean so that user can read and understand 
+  in case of about we can use (`hello ${req.query.name}`);
+  so that it will give the search part of the user and print it .
 
 
